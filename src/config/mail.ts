704631923f62bda7c77b074c769c16ac68320531
Djanilson Martins <djanlm@hotmail.com>
2020-07-24 22:40:12 -0300

    organizando container

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'email_cadastrado_na_aws@gmail.com',
+      name: 'Dj',
+    },
+  },
+} as IMailConfig;
