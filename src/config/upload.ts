6bc4ff5189e6c6125411794bc784de020a89ca5f
Djanilson Martins <djanlm@hotmail.com>
2020-07-25 06:03:30 -0300

    upload de arquivos para o Amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,47 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX'); // gera uma string hexadecimal aleatória
-      const fileName = `${fileHash}-${file.originalname}`; // O nome do arquivo salvo será a string aleatória mais o nome original
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX'); // gera uma string hexadecimal aleatória
+        const fileName = `${fileHash}-${file.originalname}`; // O nome do arquivo salvo será a string aleatória mais o nome original
 
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }), // armazena no proprio HD
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }), // armazena no proprio HD
-};
+  },
+} as IUploadConfig;
